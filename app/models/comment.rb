422e9ae7b60a544a286a27df3c7066ebf3ad7470
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-08-11 01:43:16 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-end
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      if user_attribute.present?
+        user = User.find_or_create_by(:username => user_attribute)
+        user.comments << self
+      end
+    end
+  end
+  end
